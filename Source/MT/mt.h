885c1126c4458066294a8e316f9cd54198e289a9
spinicist <tobias@spinicist.org.uk>
2019-11-07 09:31:55 +0000

    WIP Better CMake + B1

--- Source/MT/mt.h
+++ Source/MT/mt.h
@@ -1,10 +1,10 @@
 #include <functional>
 #include <map>
+#include <string>
 
 int lineshape_main(int argc, char **argv);
 int lorentzian_main(int argc, char **argv);
 int mtsat_main(int argc, char **argv);
-int main(int argc, char **argv);
 int qmt_main(int argc, char **argv);
 int ssfp_emt_main(int argc, char **argv);
 int zspec_b1_main(int argc, char **argv);
--- Source/qi_main.cpp
+++ Source/qi_main.cpp
@@ -1,23 +1,38 @@
 #include "Log.h"
 
-#ifdef BUILD_MT
+#include "B1/b1.h"
 #include "MT/mt.h"
-#endif
+
+using MainFunc = std::function<int(int, char **)>;
 
 int main(int argc, char **argv) {
 
-    std::map<std::string, std::function<int(int, char **)>> commands;
+    std::map<std::string, MainFunc> commands;
+
+#ifdef BUILD_B1
+    add_b1_commands(commands);
+#endif
 #ifdef BUILD_MT
     add_mt_commands(commands);
 #endif
-    if (argc < 2) {
+
+    auto print_command_list = [&commands]() {
         QI::Log(true, "Available commands:");
         for (auto const &kv_pair : commands) {
-            QI::Log(true, "{}", kv_pair.first);
+            QI::Log(true, "\t{}", kv_pair.first);
         }
-        QI::Fail("Must specify sub-command");
+    };
+
+    if (argc < 2) {
+        print_command_list();
+        QI::Fail("Must specify command");
     }
 
     std::string command(argv[1]);
-    return commands[command](argc - 1, argv + 1);
+    auto        find_command = commands.find(command);
+    if (find_command == commands.end()) {
+        print_command_list();
+        QI::Fail("Unknown command {}", command);
+    }
+    return find_command->second(argc - 1, argv + 1);
 }
